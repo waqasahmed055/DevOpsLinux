88b8dbce10364f994ddfd106feef6aac2bd189b5
waqasahmed055 <142661438+waqasahmed055@users.noreply.github.com>
2026-02-05 22:36:46 +0500

    added ps1 script for oci

--- scripts/oci-details.sh
+++ scripts/oci-details.sh
@@ -9,5 +9,40 @@ oci compute image get `
   --query "data.{OS:`"operating-system`",Version:`"operating-system-version`",Name:`"display-name`"}" `
   --output table
 
+#Powershell script
+```
+# Set your compartment ID
+$COMPARTMENT_ID = "ocid1.compartment.oc1..aaaaaaaa..."
+
+# Get all instances
+$instances = oci compute instance list --compartment-id $COMPARTMENT_ID | ConvertFrom-Json
+
+# Loop through each instance
+foreach ($instance in $instances.data) {
+    $instanceId = $instance.id
+    $instanceName = $instance.'display-name'
+    $instanceState = $instance.'lifecycle-state'
+    $imageId = $instance.'image-id'
+    
+    # Get IP addresses
+    $vnics = oci compute instance list-vnics --instance-id $instanceId | ConvertFrom-Json
+    $publicIp = $vnics.data[0].'public-ip'
+    $privateIp = $vnics.data[0].'private-ip'
+    
+    # Get OS information
+    $image = oci compute image get --image-id $imageId | ConvertFrom-Json
+    $os = $image.data.'operating-system'
+    $osVersion = $image.data.'operating-system-version'
+    
+    # Display information
+    Write-Host "Instance: $instanceName"
+    Write-Host "  State: $instanceState"
+    Write-Host "  Public IP: $publicIp"
+    Write-Host "  Private IP: $privateIp"
+    Write-Host "  OS: $os $osVersion"
+    Write-Host "------------------------"
+}
+```
+
 
   
